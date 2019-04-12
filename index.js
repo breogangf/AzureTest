b861d7182e8b95acbbef51adf9bd79347c738b96
Breogán González Fernández <breogangf@gmail.com>
2019-04-12 14:02:01 +0200

    Test: I can create a container

--- index.js
+++ index.js
@@ -32,21 +32,30 @@ async function showContainerNames(aborter, serviceURL) {
         response = await serviceURL.listContainersSegment(aborter, marker);
         marker = response.marker;
         for(let container of response.containerItems) {
-            console.log(` - ${ container.name }`);
+            console.log(` - ${ container.name } ✓`);
         }
     } while (marker);
 }
 
 async function execute() {
 
+    const containerName = "test";
+
     const credentials = new SharedKeyCredential(STORAGE_ACCOUNT_NAME, ACCOUNT_ACCESS_KEY);
     const pipeline = StorageURL.newPipeline(credentials);
     const serviceURL = new ServiceURL(`https://${STORAGE_ACCOUNT_NAME}.blob.core.windows.net`, pipeline);
+
+    const containerURL = ContainerURL.fromServiceURL(serviceURL, containerName);
     const aborter = Aborter.timeout(30 * ONE_MINUTE);
 
     console.log("Kinton test just started!\n");
-    console.log("I can list your containers:");
+
+    console.log("Let me list all your containers:");
     await showContainerNames(aborter, serviceURL);
+
+    console.log(`\nLet's create a new ${containerName} container`)
+    await containerURL.create(aborter);
+    console.log(` - Container: "${containerName}" was created ✓`);
 }
 
-execute().then(() => console.log("\nAll done ✓")).catch((e) => console.log(e));
+execute().then(() => console.log("\nAll done ")).catch((e) => console.log(e));
