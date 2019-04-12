c3757ea9df2788716bb69b409a6198602f65aa8f
Breogán González Fernández <breogangf@gmail.com>
2019-04-12 13:36:13 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const {
+    Aborter,
+    BlockBlobURL,
+    ContainerURL,
+    ServiceURL,
+    SharedKeyCredential,
+    StorageURL,
+    uploadStreamToBlockBlob,
+    uploadFileToBlockBlob
+} = require('@azure/storage-blob');
+
+const fs = require("fs");
+const path = require("path");
+
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+}
+
+const STORAGE_ACCOUNT_NAME = process.env.AZURE_STORAGE_ACCOUNT_NAME;
+const ACCOUNT_ACCESS_KEY = process.env.AZURE_STORAGE_ACCOUNT_NAME;
+
+const ONE_MEGABYTE = 1024 * 1024;
+const FOUR_MEGABYTES = 4 * ONE_MEGABYTE;
+const ONE_MINUTE = 60 * 1000;
+
+async function showContainerNames(aborter, serviceURL) {
+
+    let response;
+    let marker;
+
+    do {
+        response = await serviceURL.listContainersSegment(aborter, marker);
+        marker = response.marker;
+        for(let container of response.containerItems) {
+            console.log(` - ${ container.name }`);
+        }
+    } while (marker);
+}
+
+async function execute() {
+
+    const credentials = new SharedKeyCredential(STORAGE_ACCOUNT_NAME, ACCOUNT_ACCESS_KEY);
+    const pipeline = StorageURL.newPipeline(credentials);
+    const serviceURL = new ServiceURL(`https://${STORAGE_ACCOUNT_NAME}.blob.core.windows.net`, pipeline);
+    
+    const aborter = Aborter.timeout(30 * ONE_MINUTE);
+
+    console.log("Kinton test just started!\n");
+    console.log("I can list your containers:");
+    await showContainerNames(aborter, serviceURL);
+}
+
+execute().then(() => console.log("\nAll done ✓")).catch((e) => console.log(e));
